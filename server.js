621620286658287bd08b71d10c1d7c12995036af
Harish <harishbb168@gmail.com>
2023-08-01 16:56:38 +0530

    Adding cors library and setup to add headers to allow cors policy.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL);
@@ -7,7 +8,14 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
+let cors_origins = process.env.CORS_ORIGINS;
+cors_origins = cors_origins.split(";");
+const origins = { origin: cors_origins };
+console.log("Allowed origins (CORS_ORIGINS) are : ", origins);
+
 const app = express();
+app.use(cors(origins));
+
 app.use(express.json());
 
 const usersRouter = require("./routes/users");
